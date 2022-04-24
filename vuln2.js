172aed591c28e8e3e98b004540766828b1ec6409
Marynk <36852627+Marynk@users.noreply.github.com>
2022-04-24 14:23:43 -0400

    Create vuln2.js

---
+++ vuln2.js
@@ -0,0 +1,15 @@
+function test1() {
+    // ruleid: grpc-nodejs-insecure-connection
+    var grpc = require('grpc');
+
+    var booksProto = grpc.load('books.proto');
+
+    var client = new booksProto.books.BookService('127.0.0.1:50051', grpc.credentials.createInsecure());
+
+    client.list({}, function(error, books) {
+        if (error)
+            console.log('Error: ', error);
+        else
+            console.log(books);
+    });
+}
